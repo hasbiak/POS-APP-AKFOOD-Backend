c919d45358b37993429aa0d66300ec9c42f63263
hasbiak <hasbialwi70@gmail.com>
2020-10-19 21:44:10 +0700

    Update helper

--- src/helper/index.js
+++ src/helper/index.js
@@ -4,6 +4,7 @@ module.exports = {
     result.status = status || 200;
     result.msg = msg;
     result.data = data;
+    result.pagination = pagination;
 
     return response.status(result.status).json(result);
   },
